03ff667564381fde548664ec79735ce48b2a6378
yamada-michel27 <f.atsuya.personal@gmail.com>
2026-02-09 13:38:59 +0900

    Initial Commit

---
+++ anshin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  anshin
+//
+//  Created by 古田篤哉 on 2026/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
